c65d312bdfa8f34f9b2fcccda04793d51a8a6585
andrei.sadchikov <sadchikov.ae@phystech.edu>
2024-07-26 21:21:12 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import time
+from datetime import datetime, timedelta
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from src.option_data_downloader import OptionDataDownloader
 
-def fetch_moex_data(tickers, path='data'):
+def fetch_moex_data(tickers, base_path='data'):
     """Download options trading data from MOEX website and save to CSV files."""
     service = Service(GeckoDriverManager().install())
     odd = OptionDataDownloader(service=service)
+    
+    now = datetime.now()
+    rounded_minute = (now.minute // 15) * 15
+    rounded_time = now.replace(minute=rounded_minute, second=0, microsecond=0)
+    timestamp = rounded_time.strftime('%Y%m%d_%H%M')
 
     for ticker in tickers:
         print('<_______________________')
         try:
-            odd.download(ticker=ticker, dir=f'{path}/{ticker}')
+            path = f'{base_path}/{timestamp}/{ticker}'
+            odd.download(ticker=ticker, dir=path)
             print(f'Downloaded data for {ticker} successfully')
         except Exception as e:
             print(f'Error occurred when processing {ticker}: {e}')
         print('_______________________>')
 
+def wait_until_next_interval():
+    """Wait until the next 00, 15, 30, or 45 minute interval."""
+    now = datetime.now()
+    next_minute = (now.minute // 15 + 1) * 15
+    if next_minute == 60:
+        next_minute = 0
+        next_hour = now.hour + 1
+        next_time = now.replace(hour=next_hour, minute=next_minute, second=0, microsecond=0)
+    else:
+        next_time = now.replace(minute=next_minute, second=0, microsecond=0)
+    
+    wait_time = (next_time - now).total_seconds()
+    print(f"Waiting for {wait_time} seconds until the next interval.")
+    time.sleep(max(0, wait_time))
 
 if __name__ == '__main__':
     tickers = [
@@ -27,4 +48,4 @@ if __name__ == '__main__':
     
     while True:
         fetch_moex_data(tickers=tickers)
-        time.sleep(900)  # Sleep for 15 minutes (900 seconds)
+        wait_until_next_interval()
--- run_fetcher.sh
+++ run_fetcher.sh
@@ -4,12 +4,14 @@
 SCRIPT_PATH="main.py"
 
 # Path to the log file
-LOG_FILE="fetcher.log"
+LOG_FILE_STDOUT="fetcher_stdout.log"
+LOG_FILE_STDERR="fetcher_stderr.log"
 
 # Path to the PID file
 PID_FILE="fetcher.pid"
 
 # Run the Python script in the background, redirect stdout and stderr, and store the PID
-nohup python3 $SCRIPT_PATH > /dev/null 2>> $LOG_FILE &
+nohup python3 $SCRIPT_PATH > $LOG_FILE_STDOUT 2> $LOG_FILE_STDERR & echo $! > $PID_FILE
+
 echo Fetcher process $! started.
 echo $! > $PID_FILE
